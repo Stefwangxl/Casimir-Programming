a821f9c075cf763c91bb53e985344c8ae3ca62b9
Stefwangxl <stefwangxiuli@hotmail.com>
2019-11-06 13:37:28 +0100

    my fourth commit

--- test.py
+++ test.py
@@ -2,7 +2,13 @@ print('hello world!')
 print('bad day')
 import numpy as np
 def cir(x):
-    y = np.pi * x
-    return y
-print(y)
+    y_1 = np.pi * x
+    return y_1
+    print(y_1)
+
+def area(x):
+    y_2 = np.pi / 2 * x **2
+    return y_2
+    print(y_2)
+    
 
